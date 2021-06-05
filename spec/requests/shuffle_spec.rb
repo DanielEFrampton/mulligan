d06d3429b26c8d7dea266685e4e4cd10f6e31a81
Daniel Frampton <daniel.e.frampton@gmail.com>
2021-06-05 15:20:17 -0600

    Create initial shuffle endpoint spec

---
+++ spec/requests/shuffle_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /shuffle endpoint', type: :request do
+  context 'when I send a post request to the shuffle endpoint' do
+    before do
+      post_params = {
+        deck: [
+          {
+            name: "Ace of Spades"
+          },
+          {
+            name: "7 of Diamonds"
+          },
+          {
+            name: "Queen of Clubs"
+          },
+          {
+            name: "Jack of Hearts"
+          },
+          {
+            name: "2 of Clubs"
+          },
+          {
+            name: "Ace of Hearts"
+          },
+          {
+            name: "8 of Diamonds"
+          },
+          {
+            name: "9 of Diamonds"
+          }
+        ],
+        shuffles: [
+          {
+            type: "riffle",
+            repetitions: 1,
+            config: {
+              batch_size_min: 1,
+              batch_size_max: 2,
+              split_margin_min: 1,
+              split_margin_max: 2
+            }
+          }
+        ]
+      }
+      post '/shuffle', params: post_params.to_json
+    end
+
+    it 'returns a 200 response' do
+      expect(response.code).to eq '200'
+    end
+  end
+end
