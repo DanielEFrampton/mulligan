a26471fdef975c7255ccd3afd0a4c8fe95ed1341
Daniel Frampton <daniel.e.frampton@gmail.com>
2021-06-05 15:58:40 -0600

    Add original deck to response

--- app/controllers/shuffles_controller.rb
+++ app/controllers/shuffles_controller.rb
@@ -1,6 +1,8 @@
 class ShufflesController < ApplicationController
   def create
+    parsed_request_body = ActiveSupport::JSON.decode(request.body.string)
     return_object = {}
+    return_object[:deck_before] = parsed_request_body['deck']
     render json: return_object
   end
 end
